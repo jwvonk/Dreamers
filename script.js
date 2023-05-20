ce6d3a4fae0e6b00d1761934f58da6bd68c78eaf
jwvonk <jwavonk@gmail.com>
2023-05-20 15:50:57 -0700

    Initial Files

---
+++ script.js
@@ -0,0 +1,12 @@
+let config = {
+    type: Phaser.WEBGL,
+    width: 800,
+    height: 800,
+    backgroundColor: 0x214026,
+    physics: {
+        default: 'arcade'
+    },
+    scene: []
+}
+
+let game = new Phaser.Game(config);
