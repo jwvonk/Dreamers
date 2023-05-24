00c9d52e338a65bf0f0df73e3e5b6cbb037f9a2c
jwvonk <jwavonk@gmail.com>
2023-05-23 20:02:07 -0700

    Added Camera Following

--- gameplay2.js
+++ gameplay2.js
@@ -3,34 +3,62 @@ class Platform extends Phaser.Scene {
         super('platform');
     }
 
+    preload() {
+        this.load.image('bg', './assets/bg placeholder.jpg');
+        this.load.image('pc', './assets/pc_placeholder.png');
+    }
+
     create() {
-        this.border1 =this.physics.add.existing(this.add.rectangle(400, 400, 1600, 10, 0xFFFFFF), true);
-        this.border2 =this.physics.add.existing(this.add.rectangle(400, 799, 1600, 10, 0xFFFFFF), true);
-        this.player1 = this.physics.add.existing(this.add.ellipse(100, 300, 20, 40, 0xFF0000))
-        this.player1.body.setCollideWorldBounds(true);
-        this.player2 = this.physics.add.existing(this.add.ellipse(100, 700, 20, 40, 0x00FF00))
-        this.player2.body.setCollideWorldBounds(true);
+
+        this.w = this.game.config.width;
+        this.h = this.game.config.height;
+        this.s = this.game.config.width * 0.01;
+
+        this.cam1 = this.cameras.main;
+        this.cam1.setSize(this.w, this.h / 2)
+        this.cam1.setBounds(0, 0, this.w * 2, this.h / 2)
+
+        this.cam2 = this.cameras.add(0, this.h / 2, this.w, this.h / 2);
+        this.cam2.scrollY = this.h / 2;
+        this.cam2.setBounds(0, this.h/2, this.w * 2, this.h / 2)
+
+        this.add.image(0, 0, 'bg').setOrigin(0, 0)
+        this.add.image(this.w, 0, 'bg').setOrigin(0, 0)
+        this.add.image(0, 600, 'bg').setOrigin(0, 0)
+        this.add.image(this.w, 600, 'bg').setOrigin(0, 0)
+
+        this.border1 = this.physics.add.existing(this.add.rectangle(0, this.h / 2, this.w * 4, 10, 0x000000).setOrigin(0, 1), true);
+        this.border2 = this.physics.add.existing(this.add.rectangle(0, this.h, this.w * 4, 10, 0x00000).setOrigin(0, 1), true);
+        this.player1 = this.physics.add.image(150, 200, 'pc');
+        this.player2 = this.physics.add.image(150, 800, 'pc').setTintFill(0x00FF00);
+
+
+        this.cam1.startFollow(this.player1, false, 1, 1, 0, 213);
+        this.cam2.startFollow(this.player2, false, 1, 1, 0, 213);
+
+        this.cam1.setDeadzone(500, this.h / 2);
+        this.cam2.setDeadzone(500, this.h / 2);
 
         this.physics.add.collider(this.player1, this.border1);
         this.physics.add.collider(this.player2, this.border2);
+        this.physics.add.collider(this.player2, this.border1);
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.input.on('pointerdown', () => {this.scene.start('top')});
+        
     }
 
     update() {
         const { left, right, up } = this.cursors;
 
-        if (left.isDown)
-        {
-            this.player1.body.setVelocityX(-160);
-            this.player2.body.setVelocityX(-160);
+        if (left.isDown) {
+            this.player1.body.setVelocityX(-500);
+            this.player2.body.setVelocityX(-500);
 
         }
         else if (right.isDown)
         {
-            this.player1.body.setVelocityX(160);
-            this.player2.body.setVelocityX(160);
+            this.player1.body.setVelocityX(500);
+            this.player2.body.setVelocityX(500);
         }
         else
         {
@@ -41,79 +69,32 @@ class Platform extends Phaser.Scene {
 
         if (up.isDown && this.player1.body.touching.down)
         {
-            this.player1.body.setVelocityY(-330);
+            this.player1.body.setVelocityY(-600);
         }
 
         if (up.isDown && this.player2.body.touching.down)
         {
-            this.player2.body.setVelocityY(-330);
+            this.player2.body.setVelocityY(-600);
         }
 
     }
 
 }
 
-class Top extends Phaser.Scene {
-    constructor() {
-        super('top');
-    }
-
-    create() {
-        this.physics.world.gravity.y = 0;
-        this.border1 =this.physics.add.existing(this.add.rectangle(400, 400, 1600, 10, 0xFFFFFF), true);
-        this.border2 =this.physics.add.existing(this.add.rectangle(400, 799, 1600, 10, 0xFFFFFF), true);
-        this.player1 = this.physics.add.existing(this.add.ellipse(100, 300, 20, 20, 0xFF0000))
-        this.player1.body.setCollideWorldBounds(true);
-        this.player2 = this.physics.add.existing(this.add.ellipse(100, 700, 20, 20, 0x00FF00))
-        this.player2.body.setCollideWorldBounds(true);
-
-        this.physics.add.collider(this.player1, this.border1);
-        this.physics.add.collider(this.player2, this.border2);
-        this.cursors = this.input.keyboard.createCursorKeys();
-
-        this.input.on('pointerdown', () => {this.scene.start('platform')});
-    }
-
-    update() {
-        const { left, right, up, down } = this.cursors;
-
-        if (left.isDown) {
-            this.player1.body.setVelocityX(-160);
-            this.player2.body.setVelocityX(-160);
-
-        } else if (right.isDown) {
-            this.player1.body.setVelocityX(160);
-            this.player2.body.setVelocityX(160);
-        } else {
-            this.player1.body.setVelocityX(0);
-            this.player2.body.setVelocityX(0);
-        }
-        if (up.isDown) {
-            this.player1.body.setVelocityY(-160);
-            this.player2.body.setVelocityY(-160);
-        } else if (down.isDown) {
-            this.player1.body.setVelocityY(160);
-            this.player2.body.setVelocityY(160);
-        } else {
-            this.player1.body.setVelocityY(0);
-            this.player2.body.setVelocityY(0);
-        }
-    }
-}
 
 let config = {
     type: Phaser.WEBGL,
-    width: 800,
-    height: 800,
+    width: 1920,
+    height: 1200,
     backgroundColor: 0x000000,
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 300},
-            debug: false
+            gravity: {y: 1200},
+            debug: true
         }
     },
-    scene: [Platform, Top]
+    scene: [Platform]
 }
 
 let game = new Phaser.Game(config);
