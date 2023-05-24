320da53d1e4ccaccf1732ed7223c73780dd41f30
jwvonk <jwavonk@gmail.com>
2023-05-23 20:37:38 -0700

    Resized to a reasonable size

--- gameplay2.js
+++ gameplay2.js
@@ -22,27 +22,39 @@ class Platform extends Phaser.Scene {
         this.cam2.scrollY = this.h / 2;
         this.cam2.setBounds(0, this.h/2, this.w * 2, this.h / 2)
 
-        this.add.image(0, 0, 'bg').setOrigin(0, 0)
-        this.add.image(this.w, 0, 'bg').setOrigin(0, 0)
-        this.add.image(0, 600, 'bg').setOrigin(0, 0)
-        this.add.image(this.w, 600, 'bg').setOrigin(0, 0)
+        this.add.image(0, 0, 'bg').setOrigin(0, 0).setScale(.5)
+        this.add.image(this.w, 0, 'bg').setOrigin(0, 0).setScale(.5)
+        this.add.image(0, 300, 'bg').setOrigin(0, 0).setScale(.5)
+        this.add.image(this.w, 300, 'bg').setOrigin(0, 0).setScale(.5)
 
         this.border1 = this.physics.add.existing(this.add.rectangle(0, this.h / 2, this.w * 4, 10, 0x000000).setOrigin(0, 1), true);
         this.border2 = this.physics.add.existing(this.add.rectangle(0, this.h, this.w * 4, 10, 0x00000).setOrigin(0, 1), true);
-        this.player1 = this.physics.add.image(150, 200, 'pc');
-        this.player2 = this.physics.add.image(150, 800, 'pc').setTintFill(0x00FF00);
+
+        this.player1 = this.physics.add.image(75, 100, 'pc').setScale(.5)
+        this.player2 = this.physics.add.image(75, 400, 'pc').setTintFill(0x00FF00).setScale(.5);
 
 
-        this.cam1.startFollow(this.player1, false, 1, 1, 0, 213);
-        this.cam2.startFollow(this.player2, false, 1, 1, 0, 213);
+        this.cam1.startFollow(this.player1, false, 1, 1, 0, 106);
+        this.cam2.startFollow(this.player2, false, 1, 1, 0, 106);
 
-        this.cam1.setDeadzone(500, this.h / 2);
-        this.cam2.setDeadzone(500, this.h / 2);
+        this.cam1.setDeadzone(250, this.h / 2);
+        this.cam2.setDeadzone(250, this.h / 2);
 
         this.physics.add.collider(this.player1, this.border1);
         this.physics.add.collider(this.player2, this.border2);
         this.physics.add.collider(this.player2, this.border1);
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        // this.add.text(this.w-3*this.s, this.h-3*this.s, "📺")
+        // .setStyle({ fontSize: `${2 * this.s}px` })
+        // .setInteractive({useHandCursor: true})
+        // .on('pointerdown', () => {
+        //     if (this.scale.isFullscreen) {
+        //         this.scale.stopFullscreen();
+        //     } else {
+        //         this.scale.startFullscreen();
+        //     }
+        // });
 
         
     }
@@ -51,14 +63,14 @@ class Platform extends Phaser.Scene {
         const { left, right, up } = this.cursors;
 
         if (left.isDown) {
-            this.player1.body.setVelocityX(-500);
-            this.player2.body.setVelocityX(-500);
+            this.player1.body.setVelocityX(-250);
+            this.player2.body.setVelocityX(-250);
 
         }
         else if (right.isDown)
         {
-            this.player1.body.setVelocityX(500);
-            this.player2.body.setVelocityX(500);
+            this.player1.body.setVelocityX(250);
+            this.player2.body.setVelocityX(250);
         }
         else
         {
@@ -69,12 +81,12 @@ class Platform extends Phaser.Scene {
 
         if (up.isDown && this.player1.body.touching.down)
         {
-            this.player1.body.setVelocityY(-600);
+            this.player1.body.setVelocityY(-400);
         }
 
         if (up.isDown && this.player2.body.touching.down)
         {
-            this.player2.body.setVelocityY(-600);
+            this.player2.body.setVelocityY(-400);
         }
 
     }
@@ -84,8 +96,8 @@ class Platform extends Phaser.Scene {
 
 let config = {
     type: Phaser.WEBGL,
-    width: 1920,
-    height: 1200,
+    width: 1920/2,
+    height: 1200/2,
     backgroundColor: 0x000000,
     physics: {
         default: 'arcade',
