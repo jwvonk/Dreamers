eab302b9a03b075de027b42bf45031dfbad94519
jwvonk <jwavonk@gmail.com>
2023-05-29 18:51:18 -0700

    Grouped game objects, added comments

--- prototypes/vertSlice/vertSlice.js
+++ prototypes/vertSlice/vertSlice.js
@@ -9,56 +9,57 @@ class Platform extends Phaser.Scene {
     }
 
     create() {
-
+        // All dimensions/scales should be written in terms of these
         this.w = this.game.config.width;
         this.h = this.game.config.height;
         this.s = this.game.config.width * 0.01;
 
-        this.add.image(0, 0, 'bg').setOrigin(0, 0).setScale(.5)
-        this.add.image(this.w, 0, 'bg').setOrigin(0, 0).setScale(.5)
-        this.add.image(0, 300, 'bg').setOrigin(0, 0).setScale(.5)
-        this.add.image(this.w, 300, 'bg').setOrigin(0, 0).setScale(.5)
+        // Set up background image instances
+        this.add.image(0, 0, 'bg').setOrigin(0, 0) // Top left
+        this.add.image(this.w, 0, 'bg').setOrigin(0, 0) // Top Right
+        this.add.image(0, this.h / 2, 'bg').setOrigin(0, 0) // Bottom Left
+        this.add.image(this.w, this.h / 2, 'bg').setOrigin(0, 0) // Bottom Right
 
-        this.cam1 = this.cameras.main;
-        this.cam1.setSize(this.w, this.h / 2)
-        this.cam1.setBounds(0, 0, this.w * 2, this.h / 2)
+        // Set up cameras
+        this.cam1 = this.cameras.main; // Top camera
+        this.cam1.setSize(this.w, this.h / 2) // Restrict view to top half
+        this.cam1.setBounds(0, 0, this.w * 2, this.h / 2) // Prevent from following players off screen
 
-        this.cam2 = this.cameras.add(0, this.h / 2, this.w, this.h / 2);
-        this.cam2.scrollY = this.h / 2;
-        this.cam2.setBounds(0, this.h/2, this.w * 2, this.h / 2)
+        this.cam2 = this.cameras.add(0, this.h / 2, this.w, this.h / 2); // Bottom camera
+        this.cam2.scrollY = this.h / 2; // Restrict view to bottom half
+        this.cam2.setBounds(0, this.h/2, this.w * 2, this.h / 2) // Prevent from following players off screen
 
-        this.border1 = this.physics.add.existing(this.add.rectangle(0, this.h / 2, this.w * 4, 10, 0x000000).setOrigin(0, 1), true);
-        this.border2 = this.physics.add.existing(this.add.rectangle(0, this.h, this.w * 4, 10, 0x00000).setOrigin(0, 1), true);
+        // Borders for both halves
+        this.border  = this.physics.add.group({allowGravity: false, immovable: true});
+        this.border.add(this.add.rectangle(0, this.h, this.w * 4, 10, 0x00000).setOrigin(0, 1));
+        this.border.add(this.add.rectangle(0, this.h / 2, this.w * 4, 10, 0x000000).setOrigin(0, 0.5));
+        this.border.add(this.add.rectangle(0, 0, this.w * 4, 10, 0x000000).setOrigin(0, 0));
+        this.border.add(this.add.rectangle(0, 0, 10, this.h * 2, 0x000000).setOrigin(0, 0));
+        this.border.add(this.add.rectangle(this.w * 2, 0, 10, this.h * 2, 0x000000).setOrigin(1, 0));
 
-        this.obstacle = this.physics.add.existing(this.add.rectangle(this.w / 2, this.h / 2, 50, 50, 0x000FF).setOrigin(0, 1), true);
+        // Obstacles
+        this.obstacles = this.physics.add.group({allowGravity: false, immovable: true})
+        this.obstacles.add(this.add.rectangle(this.w / 2, this.h / 2, 100, 100, 0x000FF).setOrigin(0, 1));
 
-        this.player1 = this.physics.add.image(75, 100, 'pc').setScale(.5)
-        this.player2 = this.physics.add.image(75, 400, 'pc').setTintFill(0x00FF00).setScale(.5);
+        // Players
+        this.players = this.physics.add.group();
+        this.player1 = this.players.create(150, 200, 'pc')
+        this.player2 = this.players.create(150, 800, 'pc').setTintFill(0x00FF00)
 
-
+        // Tell cameras to follow players
         this.cam1.startFollow(this.player1, false, 1, 1, 0, 106);
         this.cam2.startFollow(this.player2, false, 1, 1, 0, 106);
 
+        // Allow players to move in center of screen w/o camera follow
         this.cam1.setDeadzone(250, this.h / 2);
         this.cam2.setDeadzone(250, this.h / 2);
 
-        this.physics.add.collider(this.player1, this.border1);
-        this.physics.add.collider(this.player1, this.obstacle);
-        this.physics.add.collider(this.player2, this.border2);
-        this.physics.add.collider(this.player2, this.border1);
+        // Collissions
+        this.physics.add.collider(this.players, this.border);
+        this.physics.add.collider(this.players, this.obstacles);
+
+        // Register Keyboard Controls
         this.cursors = this.input.keyboard.createCursorKeys();
-
-        // this.add.text(this.w-3*this.s, this.h-3*this.s, "📺")
-        // .setStyle({ fontSize: `${2 * this.s}px` })
-        // .setInteractive({useHandCursor: true})
-        // .on('pointerdown', () => {
-        //     if (this.scale.isFullscreen) {
-        //         this.scale.stopFullscreen();
-        //     } else {
-        //         this.scale.startFullscreen();
-        //     }
-        // });
-
         
     }
 
@@ -66,41 +67,36 @@ class Platform extends Phaser.Scene {
         const { left, right, up } = this.cursors;
 
         if (left.isDown) {
-            this.player1.body.setVelocityX(-250);
-            this.player2.body.setVelocityX(-250);
-
+            this.players.setVelocityX(-500); // Move left
         }
-        else if (right.isDown)
+        else if (right.isDown) {
+            this.players.setVelocityX(500); // Move right
+        }
+        else {
+            this.players.setVelocityX(0); // Stand Still
+        }
+
+        if (up.isDown && this.player1.body.touching.down) // Player1 Grounded Check
         {
-            this.player1.body.setVelocityX(250);
-            this.player2.body.setVelocityX(250);
+            this.player1.setVelocityY(-600); // Jump
         }
-        else
+
+        if (up.isDown && this.player2.body.touching.down) // Player2 Grounded Check
         {
-            this.player1.body.setVelocityX(0);
-            this.player2.body.setVelocityX(0);
-
+            this.player2.setVelocityY(-600); // Jump
         }
-
-        if (up.isDown && this.player1.body.touching.down)
-        {
-            this.player1.body.setVelocityY(-400);
-        }
-
-        if (up.isDown && this.player2.body.touching.down)
-        {
-            this.player2.body.setVelocityY(-400);
-        }
-
     }
-
 }
 
 
 let config = {
     type: Phaser.WEBGL,
-    width: 1920/2,
-    height: 1200/2,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1200
+    },
     backgroundColor: 0x000000,
     physics: {
         default: 'arcade',
