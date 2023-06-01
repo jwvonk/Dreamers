2c095b0a90d533822fdfc386c5d811a08a55f6fe
jwvonk <jwavonk@gmail.com>
2023-05-31 22:16:19 -0700

    Implemented art assets for p1 & bg

--- gameplayScene.js
+++ gameplayScene.js
@@ -4,10 +4,12 @@ class GameplayScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('bg', './assets/bg placeholder.jpg');
+        this.load.image('bg1', './assets/library computer desks.png');
+        this.load.image('bg2', './assets/2d library.png');
+        this.load.image('bg3', './assets/pixel art library.png');
 
         // TODO: Change file name in path
-        this.load.spritesheet("p1", "./assets/characterA-spritesheet.tps", {frameWidth: 40, frameHeight: 40});
+        this.load.spritesheet("p1", "./assets/characterA-json.png", {frameWidth: 24, frameHeight: 27});
 
         this.load.image('pc', './assets/CharacterA-0 copy.png');
         this.load.image('star', './assets/star_placeholder.png');
@@ -16,7 +18,6 @@ class GameplayScene extends Phaser.Scene {
 
     create() {
         // TODO: test each sprite in sheet by changing last number below
-        this.add.sprite(400, 300, "p1", 1);
 
         // All dimensions/scales should be written in terms of these
         this.w = this.game.config.width;
@@ -24,10 +25,12 @@ class GameplayScene extends Phaser.Scene {
         this.s = this.game.config.width * 0.01;
 
         // Set up background image instances
-        this.add.image(0, 0, 'bg').setOrigin(0, 0) // Top left
-        this.add.image(this.w, 0, 'bg').setOrigin(0, 0) // Top Right
-        this.add.image(0, this.h / 2, 'bg').setOrigin(0, 0) // Bottom Left
-        this.add.image(this.w, this.h / 2, 'bg').setOrigin(0, 0) // Bottom Right
+        for (let x = 0; x < this.w * 2; x += 600) {
+            this.add.image(x, 0, 'bg1').setOrigin(0, 0).setScale(600/768)
+            .setFlip(x % 1200 ? true : false);
+            this.add.image(x, this.h / 2, 'bg1').setOrigin(0, 0).setScale(600/768)
+            .setFlip(x % 1200 ? true : false);
+        }
 
         // Set up cameras
         this.cam1 = this.cameras.main; // Top camera
@@ -46,17 +49,16 @@ class GameplayScene extends Phaser.Scene {
         this.border.add(this.add.rectangle(0, 0, 10, this.h * 2, 0x000000).setOrigin(0, 0));
         this.border.add(this.add.rectangle(this.w * 2 + 150, 0, 10, this.h * 2, 0x000000).setOrigin(1, 0));
 
-        // all stars
-        this.stars = this.physics.add.group({allowGravity: false});
-        // Collectable stars for top half
-        this.topStars = this.physics.add.group({allowGravity: false});
-        // Collectable stars from bottom half
-        this.bottomStars = this.physics.add.group({allowGravity: false});
-
         // Players
         this.players = this.physics.add.group();
-        this.player1 = this.players.create(150, 200, 'pc').setOffset(10, 7).setScale(6).setSize(75/6, 150/6, false);
-        this.player2 = this.players.create(150, 800, 'pc').setOffset(10, 7).setScale(6).setSize(75/6, 150/6, false).setTint(0x87cdFF);
+        this.player1 = this.players.create(150, 200, 'p1').setOffset(6, 0).setScale(6).setSize(75/6, 150/6, false);
+        this.player2 = this.players.create(150, 800, 'p1').setOffset(6, 0).setScale(6).setSize(75/6, 150/6, false).setTint(0x87cdFF);
+
+        this.anims.create ({
+            key: 'player1Walk',
+            frames: this.anims.generateFrameNumbers('p1', { frames: [2, 4] }),
+            frameRate: 4,
+        });
 
         // Tell cameras to follow players
         this.cam1.startFollow(this.player1, false, 1, 1, 0, 106);
@@ -68,6 +70,13 @@ class GameplayScene extends Phaser.Scene {
 
         // Obstacles
         this.obstacles = this.physics.add.group({allowGravity: false, immovable: true});
+
+        // all stars
+        this.stars = this.physics.add.group({allowGravity: false});
+        // Collectable stars for top half
+        this.topStars = this.physics.add.group({allowGravity: false});
+        // Collectable stars from bottom half
+        this.bottomStars = this.physics.add.group({allowGravity: false});
 
         //Exit gates
         this.gates = this.physics.add.group({allowGravity: false, immovable: true});
@@ -81,13 +90,15 @@ class GameplayScene extends Phaser.Scene {
         this.physics.add.collider(this.players, this.gates);
 
         this.physics.add.overlap(this.players, this.stars, this.collectStar, null, this);
+
         // Register Keyboard Controls
         this.cursors = this.input.keyboard.createCursorKeys();
-        // console.log(this.width * 2 - 100);
+
         this.onEnter();
     }
 
-    addStar(x, y, side) {
+    // Add a collectable star at (x, y). Registered as being on `side` side.
+    addStar(x, y, side) { 
         let star = this.stars.create(x, y, 'star');
         if (side == "top") {
             this.topStars.add(star);
@@ -96,6 +107,8 @@ class GameplayScene extends Phaser.Scene {
         }
     }
 
+    // Star Collision Event Handler
+    // Opens gate on opposite side if all stars are collected on one side
     collectStar(player, star) {
         star.disableBody(true, true);
         if (this.topStars.contains(star) && this.topStars.countActive() == 0) {
@@ -112,12 +125,22 @@ class GameplayScene extends Phaser.Scene {
 
         if (left.isDown) {
             this.players.setVelocityX(-500); // Move left
+            this.player1.setFlip(true);
+            this.player1.play('player1Walk', true);
+            this.player2.setFlip(true);
+            this.player2.play('player1Walk', true);
         }
         else if (right.isDown) {
             this.players.setVelocityX(500); // Move right
+            this.player1.setFlip(false);
+            this.player1.play('player1Walk', true);
+            this.player2.setFlip(false);
+            this.player2.play('player1Walk', true);
+
         }
         else {
             this.players.setVelocityX(0); // Stand Still
+            this.player1.setFrame()
         }
 
         if (up.isDown && this.player1.body.touching.down) // Player1 Grounded Check
