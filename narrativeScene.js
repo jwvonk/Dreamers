271b9fc187d64e681d6ae3ec64c1e1cbbe02f1e2
jwvonk <jwavonk@gmail.com>
2023-06-04 22:45:34 -0700

    finished intro

--- narrativeScene.js
+++ narrativeScene.js
@@ -3,7 +3,7 @@ class NarrativeScene extends Phaser.Scene {
         super(key);
     }
     create() {
-        this.add.text(50, 200, "Click to proceed.").setFontSize(20);
+        this.add.text(50, 1150, "Click to proceed.").setFontSize(20);
         this.onEnter();
     }
     onEnter() {}
@@ -14,14 +14,14 @@ class Intro extends NarrativeScene {
         super("intro")
     }
     onEnter() {
-        this.text = this.add.text(50, 50, '\"Why am I here? Who……?\"\n\"What brought me… no… us… here?\"\n\"ho the hell are you?\"\n \"Why do I need to be trapped here with someone else?\"'
+        this.text = this.add.text(50, 50, '\"Why am I here? Who……?\"\n\n\"What brought me… no… us… here?\"\n\n\"ho the hell are you?\"\n\n\"Why do I need to be trapped here with someone else?\"'
         ).setFontSize(50);
         const fx = this.text.preFX.addReveal(.1, 0, 1);
 
         this.tweens.add({
             targets: fx,
             progress: 1,
-            duration: 3000
+            duration: 6000
         });
 
         this.input.on('pointerdown', () => this.scene.start('level1'), this);
